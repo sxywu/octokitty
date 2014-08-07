d00c93842520f61d71eaeb65c0bdb0a89e7dc0b8
Shirley Wu <shirleyxueyangwu@gmail.com>
2014-08-06 23:03:50 -0700

    hit an endpoint with pagination

--- app/assets/javascripts/views/App.View.js
+++ app/assets/javascripts/views/App.View.js
@@ -14,40 +14,49 @@ define([
 			var after = _.after(1, _.bind(this.render, this));
 			this.getEndpoint(
 				'commit',
-				'https://api.github.com/repos/mbostock/d3/commits?author=mbostock&per_page=100',
+				'https://api.github.com/repos/enjalot/checkin/commits?author=sxywu&per_page=100',
+				this.parseCommits,
 				after
 			);
 		},
-		getEndpoint: function(name, url, callback) {
+		getEndpoint: function(name, url, parse, callback) {
 			var that = this,
 				data = [];
 			if (!localStorage[name]) {
-				this.hitEndpoint(url, data);
+				this.hitEndpoint(name, url, data, parse, callback);
 		    } else {
 		        this[name] = $.parseJSON(localStorage[name]);
-		        callback();
+				callback();
 		    }
 		},
-		hitEndpoint: function(url, data) {
+		hitEndpoint: function(name, url, data, parse, callback) {
 			var that = this;
 			$.ajax({
 	        	url: url, 
 	        	success: function(response, status, request) {
-		            data.push(response);
-		            url = request.getAllResponseHeaders().match(/Link: <(.*?)>; rel="next"/);
+	        		_.each(response, function(resp) {
+	        			data.push(parse(resp));
+	        		});
+		            url = request.getAllResponseHeaders().match(/<(.*?)>; rel="next"/);
 
-		            if (url || data.length < 5) {
+		            if (url) {
 		            	url = url[1];
-		            	that.hitEndpoint(url, data);
+		            	that.hitEndpoint(name, url, data, parse, callback);
 		            } else {
-		            	that[name] = _.flatten(data);
-				        localStorage[name] = JSON.stringify(that[name]);
-				        callback();
+		            	that[name] = data;
+		            	localStorage[name] = JSON.stringify(that[name]);
+						callback();
 		            }
 		        }
 		    });
 		},
-		
+		parseCommits: function(commit) {
+			return {
+				author: commit.author.login,
+				date: commit.commit.committer.date,
+				url: commit.url
+			}
+		},
 		render: function() {
 			debugger
 		}
