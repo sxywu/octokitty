1bf4b39521d970521e524b88ce9328250ea8d5e9
allison <azywong@gmail.com>
2014-08-23 18:21:32 -0700

    only does user count for repos now

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -2,15 +2,6 @@ class ContributorsController < ApplicationController
   def show
     owner = params[:owner]
     repo = params[:repo]
-
-    searched_user = SearchCount.find_by_username(owner)
-    if searched_user
-      searched_user.count += 1
-      searched_user.save
-    else
-      searched_user = SearchCount.create(username: owner, count: 1)
-    end
-
     client = Octokit::Client.new \
       :client_id     => "#{CONFIG['github']['client_id']}",
       :client_secret => "#{CONFIG['github']['client_secret']}"
