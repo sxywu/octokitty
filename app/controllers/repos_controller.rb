240d40802a5b20f8ac0052d795cb4673a1931a01
allison <azywong@gmail.com>
2014-08-24 22:08:20 -0700

    changed secret variables to work with heroku

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -9,9 +9,11 @@ class ReposController < ApplicationController
       searched_user = SearchCount.create(username: username, count: 1)
     end
 
+    client_id = ENV['github_client_id'] || CONFIG['github']['client_id']
+    client_secret = ENV['github_client_secret'] || CONFIG['github']['client_secret']
     client = Octokit::Client.new \
-      :client_id     => "#{CONFIG['github']['client_id']}",
-      :client_secret => "#{CONFIG['github']['client_secret']}"
+      :client_id     => "#{client_id}",
+      :client_secret => "#{client_secret}"
     client.auto_paginate = true
     user = client.user "#{username}"
     repos = user.rels[:repos].get
