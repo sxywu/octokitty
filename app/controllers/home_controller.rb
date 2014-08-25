49b47afb626fc068069b5127e6501d6728f21fe0
allison <azywong@gmail.com>
2014-08-25 16:17:52 -0700

    added top searched users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,17 @@ class HomeController < ApplicationController
   def index
     @callback_url = '/oauth/callback'
     @client_id = ENV['github_client_id'] || CONFIG['github']['client_id']
+    ordered_users = SearchCount.find(:all, :order => "count desc")
+    if ordered_users.length == 0
+      @popular_users = ['sxywu']
+    elsif ordered_users.length < 5
+      @popular_users = ordered_users.map { |user| user.username }
+    else
+      @popular_users = []
+      ordered_users = ordered_users.map { |user| user.username }
+      while @popular_users.length < 5
+        @popular_users << ordered_users.shift
+      end
+    end
   end
 end
