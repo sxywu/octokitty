3c5cb5d316a1a651fb02ff8889f5010e3e20717d
Shirley Wu <shirleyxueyangwu@gmail.com>
2014-08-25 17:04:25 -0700

    some more stylin'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,16 @@ class HomeController < ApplicationController
   def index
     @callback_url = '/oauth/callback'
     @client_id = ENV['github_client_id'] || CONFIG['github']['client_id']
+
     ordered_users = SearchCount.find(:all, :order => "count desc")
     if ordered_users.length == 0
       @popular_users = ['sxywu']
-    elsif ordered_users.length < 5
+    elsif ordered_users.length < 10
       @popular_users = ordered_users.map { |user| { username: user.username, count: user.count } }
     else
       @popular_users = []
       ordered_users = ordered_users.map { |user| { username: user.username, count: user.count } }
-      while @popular_users.length < 5
+      while @popular_users.length < 10
         @popular_users << ordered_users.shift
       end
     end
