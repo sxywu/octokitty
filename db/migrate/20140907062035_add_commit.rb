d8709e47470713c35694d592fb030a90b9687a42
Shirley Wu <shirleyxueyangwu@gmail.com>
2014-09-07 03:34:14 -0700

    commits need owners too oops

--- db/migrate/20140907062035_add_commit.rb
+++ db/migrate/20140907062035_add_commit.rb
@@ -2,6 +2,7 @@ class AddCommit < ActiveRecord::Migration
   def up
     create_table :commits do |t|
       t.string :contributor
+      t.string :owner
       t.belongs_to :repo
       t.text :data
       t.timestamps
