0816eb98b883c3e3bb1460c29a7aa52bc9c750c3
allison <azywong@gmail.com>
2014-09-07 00:24:22 -0700

    fixed home controller to reflect db changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,14 @@ class HomeController < ApplicationController
     @callback_url = '/oauth/callback'
     @client_id = ENV['github_client_id'] || CONFIG['github']['client_id']
 
-    ordered_users = SearchCount.find(:all, :order => "count desc")
+    ordered_users = User.find(:all, :order => "search_count desc")
     if ordered_users.length == 0
       @popular_users = [{username: "sxywu", count: 0}]
     elsif ordered_users.length < 10
-      @popular_users = ordered_users.map { |user| { username: user.username, count: user.count } }
+      @popular_users = ordered_users.map { |user| { username: user.username, count: user.search_count } }
     else
       @popular_users = []
-      ordered_users = ordered_users.map { |user| { username: user.username, count: user.count } }
+      ordered_users = ordered_users.map { |user| { username: user.username, count: user.search_count } }
       while @popular_users.length < 10
         @popular_users << ordered_users.shift
       end
