ddf796e95985456f46dbb85eb7424e8e582018dc
Shirley Wu <shirleyxueyangwu@gmail.com>
2014-10-05 23:14:53 -0700

    add contribution.rb

---
+++ app/models/contribution.rb
@@ -0,0 +1,63 @@
+class Contribution < ActiveRecord::Base
+  belongs_to :user, primary_key: :username, foreign_key: :contributor
+  belongs_to :repo
+
+  has_many :contribution_responses
+  has_many :responses, :through => :contribution_responses, :uniq => true
+
+  attr_accessible :repo_id, :owns, :fetched
+
+   def fetch
+   	self.fetched = 'fetching'
+    self.save
+
+  	perform
+  end
+
+  def perform
+    client = ApiClient.new
+    repo = Repo.find(self.repo_id)
+    if self.commits
+      commits = client.get_commits("#{repo.owner}"+"/"+"#{repo.name}", {
+        :author => "#{self.contributor}", 
+        :since => self.updated_at,
+        :per_page => 100
+      })
+
+      if commits.count > 0
+        # concatenate old array of commits with new
+        commits = JSON.parse(self.commits) + commits
+        self.commits = commits.to_json
+      end
+    else
+      commits = client.get_commits("#{repo.owner}"+"/"+"#{repo.name}", {
+        :author => "#{self.contributor}", 
+        :per_page => 100
+      })
+
+      self.commits = commits.to_json
+    end
+    
+    self.save
+    success
+  end
+
+  def success
+  	self.fetched = 'success'
+    self.save
+
+  	self.responses.each do |response|
+      response.contributions_fetched(self)
+    end
+  end
+
+  def parse_for_render
+    repo = Repo.find(self.repo_id)
+    return JSON.parse(self.data).map do |c|
+      c[:repo] = repo.name
+      c[:owner] = repo.owner
+
+      c
+    end
+  end
+end
