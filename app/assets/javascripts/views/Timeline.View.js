10aa93d5bad6bd95dc670dc5af9c4ed581c013ad
Shirley Wu <shirleyxueyangwu@gmail.com>
2014-10-12 16:40:55 -0700

    timeline view: contributor scale

--- app/assets/javascripts/views/Timeline.View.js
+++ app/assets/javascripts/views/Timeline.View.js
@@ -17,6 +17,8 @@ define([
 		initialize: function() {
 			this.lineVisualization = new LineVisualization();
 			this.circleVisualization = new CircleVisualization();
+
+			this.contributorScale = d3.scale.ordinal();
 		},
 		render: function() {
 
@@ -32,9 +34,12 @@ define([
 				that.reposByContributors[contributor] = {
 					username: contributor
 				};
-				that.reposByContributors[contributor].repos = _.filter(repos, function(repo) {
-					return repo.owner === contributor;
-				});
+				that.reposByContributors[contributor].repos = _.chain(repos)
+					.filter(function(repo) {
+						return repo.owner === contributor;
+					}).sortBy(function(repo) {
+						return -(repo.forks + repo.stars + repo.watches);
+					}).value();
 				that.reposByContributors[contributor].popularity = _.reduce(that.reposByContributors[contributor].repos, 
 					function(memo, repo) {
 						return memo + repo.forks + repo.stars + repo.watches;
@@ -48,6 +53,29 @@ define([
 			this.calculateLines();
 		},
 		calculateLines: function() {
+			var height = $(window).height(),
+				length = Math.floor(height / app.contributorPadding),
+				range = _.chain(length).range()
+					.map(function(i) {return i * app.contributorPadding + app.padding.left}).value();
+			this.contributorScale.range(range);
+
+			this.contributorsAndRepos = [];
+
+			var i = 0,
+				that = this;
+			_.each(this.contributors, function(contributor) {
+				if (i >= length) return;
+
+				that.contributorsAndRepos.push(contributor);
+				i += 1;
+				_.each(that.reposByContributors[contributor].repos, function(repo) {
+					if (i >= length) return;
+					that.contributorsAndRepos.push(repo.owner + '/' + repo.name);
+					i += 1;
+				});
+			});
+
+			this.contributorScale.domain(this.contributorsAndRepos);
 			debugger
 		}
 	});
