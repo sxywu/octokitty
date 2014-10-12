d712bf41a26ce1de0e776ee1fd150dc32ec2a774
Shirley Wu <shirleyxueyangwu@gmail.com>
2014-10-12 16:26:29 -0700

    group repos by contributors, timeline view

--- app/assets/javascripts/views/Timeline.View.js
+++ app/assets/javascripts/views/Timeline.View.js
@@ -22,9 +22,32 @@ define([
 
 		},
 		processData: function(users, repos, commits) {
-			this.contributors = _.chain(commits)
-				.pluck('contributors')
-				.flatten().value();
+			this.contributors = _.chain(repos)
+					.pluck('contributors')
+					.flatten().unique().value();
+			this.reposByContributors = {};
+
+			var that = this;
+			_.each(this.contributors, function(contributor) {
+				that.reposByContributors[contributor] = {
+					username: contributor
+				};
+				that.reposByContributors[contributor].repos = _.filter(repos, function(repo) {
+					return repo.owner === contributor;
+				});
+				that.reposByContributors[contributor].popularity = _.reduce(that.reposByContributors[contributor].repos, 
+					function(memo, repo) {
+						return memo + repo.forks + repo.stars + repo.watches;
+					}, 0);
+			});
+
+			this.contributors = _.sortBy(this.contributors, function(contributor) {
+				return -that.reposByContributors[contributor].popularity;
+			});
+
+			this.calculateLines();
+		},
+		calculateLines: function() {
 			debugger
 		}
 	});
